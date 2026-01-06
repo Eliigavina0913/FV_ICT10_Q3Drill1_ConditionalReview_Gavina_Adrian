825ec2a7e594e389bdec43582660855673f42d27
datboidrian <adrianerickgavina@gmail.com>
2026-01-06 08:42:11 +0800

    Update main.py

--- main.py
+++ main.py
@@ -8,9 +8,11 @@ def get_average(event=None):
     s1 = max(0, min(100, float(s1))) if s1  else "error"
     s2 = max(0, min(100, float(s2))) if s2 else "error"
 
+    avg = (float(s1) + float(s2)) / 2
 
-    if s1 and s2:
-        avg = (float(s1) + float(s2)) / 2
-        document.getElementById("result").innerText = f"Average: {avg:.2f}"
+    document.getElementById("get_average").innerText = f"Average: {avg:.2f}"
+    if avg >= 75:
+        document.getElementById("result").innerText = f"✅ Passed!: {avg:.2f}"
+    
     else:
-        document.getElementById("result").innerText = "⚠️ Please enter both scores!"
+        document.getElementById("result").innerText = f"❌ Failed!: {avg:.2f}"
